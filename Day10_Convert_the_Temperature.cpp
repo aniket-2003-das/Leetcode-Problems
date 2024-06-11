4d08ffb307e47590270c9650f93b33b8bf2ebb66
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-06-11 11:43:55 +0530

    Day 10

---
+++ Day10_Convert_the_Temperature.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        vector<double> ans;
+        double Kelvin = celsius + 273.15;
+        double Fahrenheit = (celsius*1.80) + 32.00;
+
+        ans.push_back(Kelvin);
+        ans.push_back(Fahrenheit);
+        return ans;
+    }
+};
