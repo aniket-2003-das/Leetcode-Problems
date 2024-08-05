894cfa9f9736d061e7fa5875dcdc2f5b5d8f541e
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-08-05 22:39:17 +0530

    Day 65

---
+++ Day65_Search_a_2D_Matrix.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        if (n==0) return false; 
+        
+        int i=0, j=m-1;  
+
+        while (i<n && j>=0) {
+            if (matrix[i][j]==target) return true; 
+            else if (matrix[i][j]>target) j--;  
+            else i++;
+        }
+        return false;  
+    }
+};
