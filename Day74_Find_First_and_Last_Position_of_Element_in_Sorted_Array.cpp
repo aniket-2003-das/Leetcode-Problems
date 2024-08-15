19dfc93c986aa80ea1646b7176854e42cd2c439f
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-08-15 22:36:21 +0530

    Day 74

--- Day74_Find_First_and_Last_Position_of_Element_in_Sorted_Array.cpp
+++ Day74_Find_First_and_Last_Position_of_Element_in_Sorted_Array.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
+    int First(vector<int>& nums, int target, int n){
+        int low=0, high=n-1;
+        int first=-1;
+
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==target) first=mid, high=mid-1;
+            else if(nums[mid]<target) low=mid+1;
+            else high=mid-1;
+        }
+        return first;
+    }
+
+    int Last(vector<int>& nums, int target, int n){
+        int low=0, high=n-1;
+        int last=-1;
+
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==target) last=mid, low=mid+1; 
+            else if(nums[mid]<target) low=mid+1;
+            else high=mid-1;
+        }
+        return last;        
+    }
+
     vector<int> searchRange(vector<int>& nums, int target) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        int n = nums.size();
-        int start = 0, end = n - 1;
-        int first = -1, last = -1;
-        while(start <= end)
-        {
-            int mid = start + (end - start) / 2;
-            if(nums[mid] == target)
-            {
-                first = mid;
-                end = mid - 1;
-            }
-            else if(nums[mid] > target) end = mid - 1;
-            else start = mid + 1;
-        }
-        start = 0, end = n - 1;
-        while(start <= end)
-        {
-            int mid = start + (end - start) / 2;
-            if(nums[mid] == target)
-            {
-                last = mid;
-                start = mid + 1;
-            }
-            else if(nums[mid] > target) end = mid - 1;
-            else start = mid + 1;
-        }
-        return {first, last};
+        int n=nums.size();
+        vector<int> ans;
+
+        ans.push_back(First(nums, target, n));
+        ans.push_back(Last(nums, target, n));
+        return ans;
     }
 };
