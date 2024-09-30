e510d026e5f8b228f1d450e8eea5f6603d15ee42
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-09-30 23:51:36 +0530

    Day 120

---
+++ Day120_Search_a_2D_Matrix_II.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int row=0, col=m-1;
+
+        //traverse the matrix from (0, m-1):
+        while (row<n && col>=0) {
+            if (matrix[row][col]==target) return true;
+            else if (matrix[row][col]<target) row++;
+            else col--;
+        }
+        return false;
+    }
+};
