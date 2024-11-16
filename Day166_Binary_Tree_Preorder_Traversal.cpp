ccf37fea01b73bb801ca67924384524328dad4af
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-16 16:19:44 +0530

    Day 166

---
+++ Day166_Binary_Tree_Preorder_Traversal.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void Traversal(TreeNode* root, vector<int> &result){
+        if (root==NULL) return;
+        result.push_back(root->val);
+        Traversal(root->left, result);
+        Traversal(root->right, result);
+    }
+
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        Traversal(root, result);
+        return result;
+    }
+};
