b52770f5a2d684fe813feda24f5dfbcedb409a15
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2025-01-17 16:35:51 +0530

    Day 227

---
+++ Day227_Search_in_a_Binary_Search_Tree.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        int Val=val;
+        while(root!=NULL && root->val!=Val){
+            root = Val<root->val? root->left:root->right;
+        }
+        return root;
+    }
+};
