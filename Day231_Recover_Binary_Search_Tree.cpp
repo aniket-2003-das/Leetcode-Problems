ab56862db4402dc2516529d3f467698905fa07ca
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2025-01-21 00:41:33 +0530

    Day 231

---
+++ Day231_Recover_Binary_Search_Tree.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    TreeNode* first;
+    TreeNode* prev;
+    TreeNode* middle;
+    TreeNode* last;
+private:
+    void inorder(TreeNode* root){
+        if(root==NULL) return;
+        inorder(root->left);
+        if(prev!=NULL && (root->val<prev->val)){
+            if(first==NULL){
+                first=prev;
+                middle=root;
+            }
+            else last=root;
+        }
+        // Mark the node as previous
+        prev=root;
+        inorder(root->right);
+    }
+public:
+    void recoverTree(TreeNode* root) {
+        first=middle=last=NULL;
+        prev = new TreeNode(INT_MIN);
+        inorder(root);
+        if(first && last) swap(first->val, last->val);
+        else if(first && middle) swap(first->val, middle->val);
+    }
+};
