2b00065a736fc88718464585fa0b538a69695a36
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2025-04-13 23:48:22 +0530

    Day 313

---
+++ Day313_Number of_Operations_to_Make_Network_Connected.cpp
@@ -0,0 +1,68 @@
+class DisjointSet {
+public:
+    vector<int> rank, parent, size;
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        DisjointSet ds(n);
+        int extras = 0;
+        for(auto it: connections){
+            int u = it[0];
+            int v = it[1];
+            if(ds.findUPar(u)==ds.findUPar(v)) extras++;
+            else ds.unionBySize(u, v);
+        }
+        int cntc = 0;
+        for(int i=0; i<n; i++){
+            if(ds.parent[i]==i) cntc++;
+        }
+        int ans=cntc-1;
+        if(extras>=ans) return ans;
+        return -1;
+    }
+};
